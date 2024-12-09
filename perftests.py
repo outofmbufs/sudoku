1a1a6ebc4be9949749b60a17d72ec1883352b194
Neil Webber <neil@webber.com>
2024-12-09 09:12:56 -0600

    cleanups

--- perftests.py
+++ perftests.py
@@ -150,37 +150,44 @@ def timetest():
         for _ in range(x.iterations):
             ps = PuzzleSolver()
             t0 = time.time()
-            moves = ps.solve(puzz)
+            moves = next(ps.solve(puzz))
             elapsed = time.time() - t0
             fastest = min(fastest, elapsed)
 
         # this is a performance test program, not a functional test,
         # but nevertheless test/assert a few things that should be true
+        if not ps.timedout:
 
-        # 1) carry out the moves that got returned
-        for m in moves:
-            puzz.move(m, autosolve=True)
+            # 1) carry out the moves that got returned
+            for m in moves:
+                puzz.move(m, autosolve=True)
 
-        #    ... which should bring the puzzle to a solution
-        assert puzz.endstate, "Puzzle did not get solved!!"
+            #    ... which should bring the puzzle to a solution
+            assert puzz.endstate, "Puzzle did not get solved!!"
 
-        # 2) verify that solution against the "known" solution
-        for r, row in enumerate(x.solution):
-            for c, elem in enumerate(row):
-                assert puzz.grid[(r, c)].value == elem, "solution mismatch"
+            # 2) verify that solution against the "known" solution
+            for r, row in enumerate(x.solution):
+                for c, elem in enumerate(row):
+                    assert puzz.grid[(r, c)].value == elem, "solution mismatch"
 
         if elapsed < 1.0:
             estr = f" {elapsed*1000:.2f} milliseconds"
         else:
             estr = f" {elapsed:.2f} seconds"
 
-        if ps.stats.moves > 100:
-            mstr = f"; {(elapsed / ps.stats.moves)*1000:.2f} msec/move"
-            mstr += f", {ps.stats.moves} moves examined"
+        if ps.timedout:
+            mstr = "; no solution found -- timed out."
+        elif ps.stats.moves > 10:
+            mstr = f"; {(elapsed / ps.stats.moves)*1000:.2f} msec/move,"
         else:
-            mstr = ""
+            mstr = f"; Only"
+        mstr += f" {ps.stats.moves} moves examined"
 
-        print(f"Case {i}:{estr}{mstr}")
+        nsolstr = f"{len(moves)} move"
+        if len(moves) != 1:
+            nsolstr += "s"
+
+        print(f"Case {i}:{estr}{mstr}. Solution: {nsolstr}.")
 
 
 if __name__ == "__main__":
